c0d3f89e55b8749f38edcedd2d2457b1b0235ab9
hqApril <forhyj@126.com>
2017-11-14 16:59:42 +0800

    hyj 11/14 16:59
    init-4

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -4,8 +4,23 @@ namespace app\index\controller;
 use think\Controller;
 
 class Index extends Controller {
-    public function index()
-    {
+    public function index() {
+        return $this -> fetch();
+    }
+
+    public function taxi() {
+        return $this -> fetch();
+    }
+
+    public function privateTaxi() {
+        return $this -> fetch();
+    }
+
+    public function aboutUs() {
+        return $this -> fetch();
+    }
+
+    public function contactUs() {
         return $this -> fetch();
     }
 }
