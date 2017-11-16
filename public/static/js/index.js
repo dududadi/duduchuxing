69ae031d55dab458d518b2c28cef2c6febb74014
pandaljf <477576263@qq.com>
2017-11-16 12:01:49 +0800

    ljf 11/16 12:01
    the finally of index

--- public/static/js/index.js
+++ public/static/js/index.js
@@ -4,13 +4,28 @@ $('#ljf .main').fullpage({
 setInterval(function(){
     $.fn.fullpage.moveSlideRight();
 }, 2000);
-function rebuild() {
-    var $boxs = $('.client .box');
-    var winW = $(window).width();
-    $boxs.css({width:winW*.8});
-}
-rebuild();
-$(window).resize(function (e) {
-    e.stopPropagation();
+window.onload = function() {
+    function rebuild() {
+        var $boxs = $('.client .box');
+        var winW = $(window).width();
+        var winH = $(window).height();
+        var decrease_w = winW*.8;
+        $boxs.width(decrease_w);
+        var $slideInner = $('#ljf #myCarousel .carousel-inner');
+        var $slideInnerTop = 226;
+        if (winW >970) {
+            $slideInner.height('420px');
+        } else {
+            var newHeight = winH - $slideInnerTop - 20;
+            var halfHeight = newHeight/2;
+            $slideInner.css({height:newHeight});
+            $slideInner.find('.item').children().css({height:halfHeight});
+        }
+
+    }
     rebuild();
-});
+    $(window).resize(function (e) {
+        e.stopPropagation();
+        rebuild();
+    });
+};
