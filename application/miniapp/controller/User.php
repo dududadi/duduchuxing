4d629af40af7089cc3b1c63584e17ca4dd41f5f0
hqApril <forhyj@126.com>
2017-11-20 22:46:13 +0800

    hyj 11/20 22:46
    接口测试

---
+++ application/miniapp/controller/User.php
@@ -0,0 +1,17 @@
+<?php
+namespace app\miniapp\controller;
+
+use think\Controller;
+use think\Db;
+use think\Paginator;
+use think\Request;
+use think\Session;
+
+class User extends Controller {
+    public function index() {
+        $res = Db::name('user')
+        -> select();
+
+        dump($res);
+    }
+} 
