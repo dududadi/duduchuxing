f7446e2473cc0ff58411120321598e77f1016936
hyyyyyy <245893478@qq.com>
2017-11-21 16:11:21 +0800

    hy 11/21 16：10
    添加map控制器

---
+++ application/miniapp/controller/Map.php
@@ -0,0 +1,28 @@
+<?php
+namespace app\miniapp\controller
+
+use think\Controller;
+use think\Db;
+use think\Session;
+use think\Cookie;
+class Map extends Controller
+{
+	function getPath()
+	{
+		dump(input("post.latitude"));
+	}
+	function calculate()
+	{
+		
+	}
+	function getDistance($lat1, $lng1, $lat2, $lng2){   
+		$radLat1=deg2rad($lat1);//deg2rad()函数将角度转换为弧度
+	    $radLat2=deg2rad($lat2);
+	    $radLng1=deg2rad($lng1);
+	    $radLng2=deg2rad($lng2);
+	    $a=$radLat1-$radLat2;
+	    $b=$radLng1-$radLng2;
+	    $s=2*asin(sqrt(pow(sin($a/2),2)+cos($radLat1)*cos($radLat2)*pow(sin($b/2),2)))*6378.137;
+	    return $s;   
+	}   
+?>
