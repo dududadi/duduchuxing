3259a02228f3eb021ecd20fc6f451a02cd281962
hyyyyyy <245893478@qq.com>
2017-11-21 16:26:27 +0800

    hy 11/21
    }

--- application/miniapp/controller/Map.php
+++ application/miniapp/controller/Map.php
@@ -1,18 +1,19 @@
 <?php
-namespace app\miniapp\controller
+namespace app\miniapp\controller;
 
 use think\Controller;
+use think\Db;
 class Map extends Controller
 {
-	function getPath()
+	public function getPath()
 	{
 		dump(input("post.latitude"));
 	}
-	function calculate()
+	public function calculate()
 	{
 		
 	}
-	function getDistance($lat1, $lng1, $lat2, $lng2){   
+	public function getDistance($lat1, $lng1, $lat2, $lng2){   
 		$radLat1=deg2rad($lat1);//deg2rad()函数将角度转换为弧度
 	    $radLat2=deg2rad($lat2);
 	    $radLng1=deg2rad($lng1);
@@ -21,5 +22,6 @@ class Map extends Controller
 	    $b=$radLng1-$radLng2;
 	    $s=2*asin(sqrt(pow(sin($a/2),2)+cos($radLat1)*cos($radLat2)*pow(sin($b/2),2)))*6378.137;
 	    return $s;   
-	}   
+	}
+}   
 ?>
