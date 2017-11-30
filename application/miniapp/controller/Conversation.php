6e7671ae5f3a762f0277730e589cbbcc7a667de3
planchet <33612803+planchet@users.noreply.github.com>
2017-11-30 19:10:34 +0800

    ycw 11/30 19:09
    更新了订单查询字段
    和更新了获取并完好保存accesstoken

--- application/miniapp/controller/Conversation.php
+++ application/miniapp/controller/Conversation.php
@@ -11,8 +11,8 @@ namespace app\miniapp\controller;
 use think\Controller;
 
 define("TOKEN",'maygodblessus');
-define("USERAPPID",'wx870f25b8a2a98f0b');
-define("USERAPPSECRET",'d51063fe3c3b3f30688c74f1f86ab768');
+define("APPID",'wx870f25b8a2a98f0b');
+define("APPSECRET",'d51063fe3c3b3f30688c74f1f86ab768');
 define('TULINGAPIKEY','186d105734dd42dd9a8e3f4607a873d4');
 class Conversation extends Controller
 {
@@ -115,6 +115,40 @@ class Conversation extends Controller
 					</xml>';
 
 
+    }
+
+    /*小程序获取access_token*/
+    public function getAccessToken()
+    {
+        // access_token 应该全局存储与更新，以下代码以写入到文件中做示例
+        $data = json_decode($this->get_php_file("access_token.php"));
+        if ($data->expire_time < time()) {
+            // 如果是企业号用以下URL获取access_token
+            // $url = "https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=$this->appId&corpsecret=$this->appSecret";
+            $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=".APPID."&secret=".APPSECRET;
+            $res = json_decode(curlHttp($url,[]));  //转换成json格式
+            $access_token = $res->access_token;     //获取其中的access_token
+            if ($access_token) {
+                //如果获取到access_token，做一个时间增加，并储存至文件
+                $data->expire_time = time() + 7000;
+                $data->access_token = $access_token;
+                $this->set_php_file("access_token.php", json_encode($data));
+            }
+        } else {
+            $access_token = $data->access_token;
+        }
+        return $access_token;
+
+    }
+
+    public function set_php_file($filename, $content) {
+        $fp = fopen($filename, "w");                //用写入方式打开文件
+        fwrite($fp, "<?php exit();?>" . $content);//写入标识加上数据
+        fclose($fp);                                //关闭资源
+    }
+
+    public function get_php_file($filename) {
+        return trim(substr(file_get_contents($filename), 15));//截取标识，获取标识后的数据
     }
 
 }
