187a48850d9b1fa126dfcf21967c47444213a8f2
pandaljf <477576263@qq.com>
2017-12-04 16:37:38 +0800

    ljf 12/04 16:37
    测试手机号修改

--- application/miniapp/controller/Driver.php
+++ application/miniapp/controller/Driver.php
@@ -462,15 +462,15 @@ class Driver extends Controller{
         $openid = Request::instance()->post('openId');
 
         //手机号验证
-//        if (!preg_match("/^1[3|4|5|8][0-9]\d{8}$/", $tel)) {
-//            echo 0;
-//            exit;
-//        }
+        if (!preg_match("/^1[3|4|5|8][0-9]\d{8}$/", $tel)) {
+            echo 0;
+            exit;
+        }
         //手机号是否注册
         $telTest = Db::name('driver')
             -> where('driv_tel', $tel)
             -> field('driv_tel')
-            -> select();
+            -> find();
 
         if ($telTest) {
             echo 1;
