d0a47bbe83a553c73ee52ee502ca7f5bebc0c301
planchet <33612803+planchet@users.noreply.github.com>
2017-12-04 12:05:39 +0800

    ycw 12/04 12:05
    不改了

--- application/miniapp/controller/Conversation.php
+++ application/miniapp/controller/Conversation.php
@@ -116,10 +116,10 @@ class Conversation extends Controller
             $msg=[
                 "touser"=>"$postObj->FromUserName",           //用户openid
                 "msgtype"=>"text",                           //类型是文字
-                "text"=> ["content"=>$resMsg->text]         //图灵回复的消息
+                "text"=> ["content"=>"$resMsg->text"]         //图灵回复的消息
             ];
             Db::name('test_chat')->insert(['tc_id'=>null,'tc_text'=>'准备回复给用户的信息:'.json_encode($msg)]);
-            $res=curlHttp($url,$msg);        //发送回微信小程序*/
+            $res=curlHttp($url,json_encode($msg));        //发送回微信小程序*/
 
             Db::name('test_chat')->insert(['tc_id'=>null,'tc_text'=>'发送后的回调:'.json_encode($res)]);
         }
