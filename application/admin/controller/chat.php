5150fd1f61d309caab6d7209ef30f2f32a5e8e37
hjw <294466789@qq.com>
2017-12-05 17:22:50 +0800

    hjw 17/12/05 17:22 后台增加客服模块 添加一个客服聊天页面

---
+++ application/admin/controller/chat.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: huangjianwu
+ * Date: 2017/12/5
+ * Time: 17:19
+ */
+namespace app\admin\controller;
+
+use think\Controller;           //引用官方封装的控制类
+class Chat extends Controller {
+    public function _initialize() {
+        if (sessionAssist('isLogin')) {
+            $this -> redirect('Index/index');
+        } else if (cookieAssist('isLogin')) {
+            //判断当前用户是否存在凭证，有则直接跳转主页
+            $user=cookie('isLogin');
+
+            //将当前用户ID存入session中
+            Session::set('isLogin',$user);
+
+            $this -> redirect('Index/index');
+        }
+
+    }
+}
