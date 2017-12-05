b5f87c6494764c90586d04b3a1677fc5821e1f16
hjw <294466789@qq.com>
2017-12-05 17:04:17 +0800

    hjw 17/12/05 17:03 首页增加客服模块 修改chat.js文件,动态生成一个iframe

--- public/static/js/chat.js
+++ public/static/js/chat.js
@@ -3,4 +3,11 @@
  */
 openChat = function () {
     window.location.href="http://www.hjw123.xin:55151/";
+    var newFrame = document.createElement("iframe");
+    newFrame.src ="http://www.hjw123.xin:55151/";
+    newFrame.frameBorder = 0;//FF、IE隐藏边框有效
+    newFrame.width = "400px";
+    newFrame.height = "400px";
+    newFrame.scrolling = "no";
+    document.body.appendChild(newFrame);
 }
