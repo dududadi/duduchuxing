873f67b455604e9a9d31f356f62b405eefa336b4
hjw <294466789@qq.com>
2017-12-05 17:17:14 +0800

    hjw 17/12/05 17:12 首页增加客服模块 修改chat.js文件,新开一个窗口

--- public/static/js/chat.js
+++ public/static/js/chat.js
@@ -2,13 +2,5 @@
  * Created by huangjianwu on 2017/11/30.
  */
 openChat = function () {
-    //window.location.href="http://www.hjw123.xin:55151/";
-    var newFrame = document.getElementsByTagName("iframe");
-
-    newFrame.src ="http://www.hjw123.xin:55151/";
-    newFrame.frameBorder = 0;//FF、IE隐藏边框有效
-    newFrame.width = "400px";
-    newFrame.height = "400px";
-    newFrame.scrolling = "no";
-    document.body.appendChild(newFrame);
+    window.open("http://www.hjw123.xin:55151/");
 }
