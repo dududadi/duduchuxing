f3d97abc4bc1d1791457e8b367e99ce3a35fd1b8
hjw <294466789@qq.com>
2017-12-05 17:41:38 +0800

    hjw 17/12/05 17:41 后台修改客服聊天的控制器，页面重定向

--- application/admin/controller/chat.php
+++ application/admin/controller/chat.php
@@ -11,7 +11,7 @@ use think\Controller;           //引用官方封装的控制类
 class Chat extends Controller {
     public function _initialize() {
         if (sessionAssist('isLogin')) {
-            $this -> redirect('Chat/chat');
+            $this -> redirect('http://www.hjw123.xin:55151/',302);
         } else if (cookieAssist('isLogin')) {
             //判断当前用户是否存在凭证，有则直接跳转主页
             $user=cookie('isLogin');
@@ -19,8 +19,15 @@ class Chat extends Controller {
             //将当前用户ID存入session中
             Session::set('isLogin',$user);
 
-            $this -> redirect('Chat/chat');
+            $this -> redirect('http://www.hjw123.xin:55151/',302);
         }
 
+
+    }
+    //显示用户列表
+    public function lists() {
+
+
+        return $this -> fetch();
     }
 }
