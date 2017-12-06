9cdcac33243761d2ee810d9df3bc601349bdab86
hyyyyyy <245893478@qq.com>
2017-12-06 13:44:46 +0800

    hy 12/6 13:44
    调试

--- application/miniapp/controller/Bill.php
+++ application/miniapp/controller/Bill.php
@@ -13,7 +13,8 @@ class Bill extends Controller {
 		$openid=Request::instance()-> post('openid');
 		$list = DB::name('driver')
 					->alias('d')//给表起别名
-			->join('driv_money_record dmr','dmr.driv_id = d.driv_id')		//联表查询 司机钱包记录表
+			->join('driv_money_record dmr','dmr.driv_id = d.driv_id')
+			->join('d_user u','u.user_id = dmr.user_id')		//联表查询 司机钱包记录表
             ->where('d.open_id',$openid)			//将微信开放ID作为凭证向数据库查询信息
 			->where('dmr_result','成功')			//获取结果为成功的钱包记录
             ->field([
@@ -29,10 +30,11 @@ class Bill extends Controller {
 	public function userBill()
 	{
 		$openid=Request::instance()-> post('openid');
-		$list = DB::name('driver')
-					->alias('d')//给表起别名
-			->join('driv_money_record dmr','dmr.driv_id = d.driv_id')		//联表查询 司机钱包记录表
-            ->where('d.open_id',$openid)			//将微信开放ID作为凭证向数据库查询信息
+		$list = DB::name('user')
+					->alias('u')//给表起别名
+			->join('driv_money_record dmr','dmr.user_id = d.user_id')		//联表查询 司机钱包记录表
+            ->join('d_driver d','d.driv_id = dmr.driv_id')
+            ->where('u.open_id',$openid)			//将微信开放ID作为凭证向数据库查询信息
 			->where('dmr_result','成功')			//获取结果为成功的钱包记录
             ->field([
                 'dmr_time' => 'dmrTime',			//结款时间
