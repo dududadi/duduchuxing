a33d7e363bfffc26c6dcfa81e599c15e05090111
hjw <294466789@qq.com>
2017-12-06 10:24:18 +0800

    hjw 17/12/06 10:08 后台修改insert2.sql文件的客服跳转模块

--- public/private/insert2.sql
+++ public/private/insert2.sql
@@ -86,7 +86,7 @@ INSERT INTO d_smenu VALUES
 (null,'订单统计','admin/Chart/orderList',  8),
 (null,'用户统计','admin/Chart/user',       8),
 (null,'营销统计','admin/Chart/market',     8),
-(null,'客服聊天','http://www.hjw123.xin:55151/',9),
+(null,'客服聊天','admin/Chat/chat',9),
 (null,'快车规则','admin/Rule/car',10),
 (null,'出租车规则','admin/Rule/taxi',10);
 
